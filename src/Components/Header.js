3a937e1051e7b18e447ec93da3beacc5a75f0f19
Francis Thibault <francis.thibault@gsoft.com>
2021-04-27 21:57:07 -0400

    added more data / fixed some ui

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ function Header() {
       <Nav/>
       <h1 className="title"><span className="title-light">An history of Nintendo</span><br />Pocket Consoles</h1>
       <hr />
-      <h2 className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et orci et purus luctus sollicitudin. Aenean posuere maximus lorem, vitae laoreet quam lacinia et. Mauris quis auctor ante, non vulputate nisi.</h2>
+      <h2 className="subtitle">It's a decade since the Nintendo 3DS first went on sale in Japan and a lot has happened both before and since!</h2>
     </>
     );
 }
